2d4701a0d256bb152c5ddacb056be1f062a8fa5c
ChrisCarr <chris.a.carr@gmail.com>
2015-06-19 16:57:19 -0500

    complete program

---
+++ BubbleSortAge/src/BubbleSortAge.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+public class BubbleSortAge {
+
+    public static void main(String[] args) {
+
+        int age[] = new int[]{32, 25, 56, 12, 20, 22, 19, 54, 56, 22};
+
+        for (int i = 0; i < age.length - 1; i++) {
+            for (int j = 1; j < age.length - i; j++) {
+                if (age[j - 1] > age[j]) {
+                    int temp = age[j - 1];
+                    age[j - 1] = age[j];
+                    age[j] = temp;
+                }
+
+            }
+
+        }
+
+        System.out.println("The sorted ages are: " + Arrays.toString(age));
+    }
+}
